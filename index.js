6a3030c455e5b0773cdd40602d97793d204c18bd
Kat <katckime@gmail.com>
2021-01-28 22:47:39 -0800

    Updating gitignore and adding package.json

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+ * Name:          Kat Kime
+ * Date:          January 28, 2021
+ * Description:   A JS file to handle all of the get/post requests recieved on
+ *                the index page.
+*/
