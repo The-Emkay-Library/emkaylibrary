9e0cc5cd6df39f7568cd6b1b8d0deb46325273ed
Kat <katckime@gmail.com>
2021-01-29 11:01:26 -0800

    Adding Handlebars

--- index.js
+++ index.js
@@ -7,11 +7,25 @@
 
 
 /*
- * Setting up Express
+ * Setting up Express & Handlebars
 */
 
 var express = require('express');
+var exphbs = require('express-handlebars');
+
 var app = express();
+var hbs = exphbs.create({defaultLayout: 'main'});
+
+// Registering hbs.engine with Express app
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Query parser
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// Setting up a folder for static files
+app.use(express.static('public'));
 
 // Setting the port
 app.set('port', 3000);
@@ -19,7 +33,27 @@ app.set('port', 3000);
 
 // Setting up a simple hello world statement to test environment
 app.get('/', function(req, res) {
-  res.send('Hello world!');
+  context = {};
+  res.render('home', context);
+});
+
+
+
+/*
+ * Handling Errors
+ */
+
+app.use(function(request, response) {
+  response.type('text/plain');
+  response.status(404);
+  response.send('404 - Not Found');
+});
+
+app.use(function(err, request, response, next) {
+  console.error(err.stack);
+  response.type('text/plain');
+  response.status(500);
+  response.send('500 - Server Error');
 });
 
 // Adding a listener
