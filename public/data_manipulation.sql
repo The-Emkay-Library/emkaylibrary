6f6ac893e9b2b128bab37e948ad779f507eba815
Kat <katckime@gmail.com>
2021-02-13 13:37:45 -0800

    Adding Initial Data Manipulation File
    Just the bare bones outline for the Data Manipulation portion of the assignment.

---
+++ public/data_manipulation.sql
@@ -0,0 +1,74 @@
+-- Name: Kat Kime
+-- Date: February 13, 2021
+-- Description: Represents the SQL queries that will be used for CRUD
+-- functionalities.
+
+
+-- SECTION: Checkouts
+
+-- PAGE: Checkout & Return Items
+-- Checkout a new item
+
+-- Return a checked-out item
+
+-- PAGE: Albumm Rentals
+-- View all album rentals
+
+-- PAGE: Book Rentals
+-- View all book rentals
+
+-- PAGE: Movie Rentals
+-- View all movie rentals
+
+
+
+-- SECTION: Items
+
+-- PAGE: Albums
+-- Search albums by title
+
+-- Add a new album
+
+-- Update an existing album
+
+-- Delete an existing album
+
+-- PAGE: Books
+-- Search books by title
+
+-- Add a new book
+
+-- Update an existing book
+
+-- Delete an existing book
+
+-- PAGE: Movies
+-- Search movies by title
+
+-- Add a new movie
+
+-- Update an existing movie
+
+-- Delete an existing movie
+
+
+
+-- SECTION: Members
+
+-- PAGE: Artists
+-- Search artists by last name
+
+-- Add a new artsist
+
+-- Edit an exisitng artist
+
+-- Delete an existing artist 
+
+-- PAGE: Patrons
+-- Search patrons by last name
+
+-- Add a new patron
+
+-- Edit an exisitng patron
+
+-- Delete an existing patron
