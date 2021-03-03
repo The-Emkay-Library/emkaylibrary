3cbe094a0fa8c4277bcc6f660c1fbbd940790d51
MerryKaur <59616406+MerryKaur@users.noreply.github.com>
2021-03-03 16:46:17 -0600

    Create deleteBook.js

---
+++ public/deleteBook.js
@@ -0,0 +1,12 @@
+function deleteBook(id){
+
+    $.ajax({
+
+        url: '/books/' + id,
+        type: 'DELETE',
+
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
