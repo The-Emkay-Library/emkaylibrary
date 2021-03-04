3c5198f357237ad212cb52631982c58df126f8f8
MerryKaur <59616406+MerryKaur@users.noreply.github.com>
2021-03-03 20:22:46 -0600

    Update movies.js

--- scripts/movies.js
+++ scripts/movies.js
@@ -32,6 +32,26 @@ module.exports = function(){
     }
   });
 
+  // DELETE functionality for movies page
+  router.delete('/:id', function (req, res) {
+    var mysql = req.app.get('mysql');
+    var sql = "DELETE FROM Movies WHERE Movie_ID = ?;";
+    var inserts = [req.params.id];
+
+    sql = mysql.pool.query(sql, inserts, function (error, results, fields) {
+      if (error) {
+        console.log(error);
+
+        res.write(JSON.stringify(error));
+        res.status(400);
+        res.end();
+
+      } else {
+        res.status(202).end();
+      }
+    })
+  })
+  
   // POST route for movies
   router.post('/', function(req, res) {
     console.log(req.body);
